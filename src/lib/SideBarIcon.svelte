fb1dc3ed486454fe5555e414426d3b04d2b3272f
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-02-20 17:30:29 +0100

    tooltips visible by default

--- src/lib/SideBarIcon.svelte
+++ src/lib/SideBarIcon.svelte
@@ -1,6 +1,10 @@
 <script>
     import Icon from '@iconify/svelte';
     export let iconName;
+    export const text = 'tooltip'
 
 </script>
-<Icon icon="{iconName}" class="sidebar-icon"/>
+<div class="relative">
+    <Icon icon="{iconName}" class="sidebar-icon" />
+    <span class="sidebar-tooltip">{text}</span>
+</div>

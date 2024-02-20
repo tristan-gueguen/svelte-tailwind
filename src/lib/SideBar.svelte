207b391252f146ac30f73f19a61ff1d8c82ba598
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-02-20 17:12:36 +0100

    choosing icons

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,7 +1,10 @@
+<script>
+	import SideBarIcon from "./SideBarIcon.svelte";
+
+</script>
 <div class="text-4xl fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
-    <i>A</i>
-    <i>B</i>
-    <i>C</i>
-    <i>D</i>
-    <i>E</i>
+    <SideBarIcon iconName="fa6-solid:fire"/>
+    <SideBarIcon iconName="bi:plus" />
+    <SideBarIcon iconName="bi:lightning-fill" />
+    <SideBarIcon iconName="bi:gear-fill" />
 </div>
---
+++ src/lib/SideBarIcon.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Icon from '@iconify/svelte';
+    export let iconName;
+
+</script>
+<Icon icon="{iconName}" class="sidebar-icon"/>

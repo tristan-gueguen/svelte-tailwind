dee8ac489f8c5bcaa9c1c7b9dd2cf27eab43598d
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-02-20 17:21:45 +0100

    style icons

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -2,7 +2,7 @@
 	import SideBarIcon from "./SideBarIcon.svelte";
 
 </script>
-<div class="text-4xl fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
+<div class="text-4xl fixed top-0 left-0 h-screen w-[100px] flex flex-col bg-gray-900 text-white shadow-lg">
     <SideBarIcon iconName="fa6-solid:fire"/>
     <SideBarIcon iconName="bi:plus" />
     <SideBarIcon iconName="bi:lightning-fill" />
